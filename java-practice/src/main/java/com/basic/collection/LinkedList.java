19c8b5401661f428ee2d843f95856388bf6c4ac2
Yongdi Wang <yongdi.wang1227@gmail.com>
2021-10-24 20:42:25 +0100

    Ver 4

---
+++ java-practice/src/main/java/com/basic/collection/LinkedList.java
@@ -0,0 +1,425 @@
+package com.basic.collection;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Iterator;
+/**
+ * Class for implementing linked list methods
+ *
+ * @author Yongdi Wang
+ *
+ */
+
+public class LinkedList<T> implements Iterable<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    /**
+     * Class for implementing Node in linked list
+     */
+     private static class Node<T> {
+        private T data;
+        private Node<T> prev;
+        private Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+        }
+
+        /**
+         * Get data of current node
+         *
+         * @return current Node's data
+         */
+        public T getData() {
+            return data;
+        }
+
+        /**
+         * Get data of previous node
+         *
+         * @return previous Node's data
+         */
+        public Node<T> getPrev() {
+            return prev;
+        }
+
+        /**
+         * Get data of next node
+         *
+         * @return next Node's data
+         */
+        public Node<T> getNext() {
+            return next;
+        }
+    }
+
+    /**
+     *
+     * @return Returns an CustomListIterator over elements of type T.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new CustomListIterator<T>(this);
+    }
+
+    /**
+     * Custom ListIterator over elements in the linkedlist of type T
+     */
+    private class CustomListIterator<T> implements Iterator<T> {
+        // if Node is not inner class, can be Node<T>
+        // if Node is inner class, then LinkedList<T>.Node<T>
+        // or make it static inner class, then can be LinkedList.Node<T>
+        // maybe because T in Node doesnt dependent on LinkedList's T
+        LinkedList.Node<T> current;
+
+        /**
+         * Constructor
+         * @param list
+         */
+        public CustomListIterator(LinkedList<T> list) {
+            current = list.getHead();
+        }
+
+        /**
+         *
+         * @return true if current node has next
+         */
+        @Override
+        public boolean hasNext() {
+            return current.getNext() != null;
+        }
+
+        /**
+         *
+         * @return true if current node has previous
+         */
+        public boolean hasPrev() {
+            return current.getPrev() != null;
+        }
+
+        /**
+         *
+         * @return data of next Node
+         */
+        @Override
+        public T next() {
+            current = current.getNext();
+            T data;
+            if (current != null) {
+                data = current.getData();
+            } else {
+                throw new NoSuchElementException("No Such Element");
+            }
+            return data;
+        }
+
+        /**
+         *
+         * @return data of previous Node
+         */
+        public T prev() {
+            current = current.getPrev();
+            T data;
+            if (current != null) {
+                data = current.getData();
+            } else {
+                throw new NoSuchElementException("No Such Element");
+            }
+            return data;
+        }
+    };
+
+
+    @Override
+    public String toString() {
+        for (int i = 0; i < this.getSize(); i++) {
+            System.out.println(this.getNode(i).get());
+//            System.out.println(this.getNode(i).get().hashCode());
+        }
+        return "Finished";
+    }
+
+    /**
+     * Check if linked list is empty
+     */
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public Node<T> getTail() {
+        return tail;
+    }
+
+    /**
+     * Add Node node at the last linked list.
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * Get node at the index
+     *
+     * @param index
+     * @return the node at index
+     */
+    public Optional<T> getNode(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Invalid Index");
+        }
+        if (index + 1 > getSize()) {
+            throw new IndexOutOfBoundsException("Index Exceed List Length");
+        }
+        Node<T> current = head;
+        // while not reach to end of the list
+        while (current != null) {
+            if (index == 0) {
+                break;
+            }
+            current = current.next;
+            index--;
+        }
+        return Optional.ofNullable(current.getData());
+    }
+
+    /**
+     * Get middle node of the linked list
+     *
+     * @return middle node of the list if the size is odd, further middle node of the list if the size is even.
+     * i.e. length = 3, 0->1->2, middle node index = 1.
+     *      length = 4, 0->1->2->3, middle node index = 2.
+     */
+    public Optional<T> getMiddle() {
+        Node<T> fast = head;
+        Node<T> slow = head;
+        // if list is empty then return head => null
+        // if not, then return head => middle node
+        // when fast points to null (finish the list) or points to last node of the list.
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return Optional.ofNullable(slow.getData());
+    }
+
+    /**
+     * Add Node node at the tail of the linked list.
+     *
+     * <p>Use {@link #addLast(Object data)} to add at the last linked list.
+     *
+     * @param data to add at the last of the list
+     * @return true if added successfully
+     */
+    public boolean addLast(T data) {
+        Node<T> node = new Node(data);
+        if(isEmpty()) {
+            head = node;
+        } else {
+            node.prev = tail;
+            tail.next = node;
+        }
+        tail = node;
+        size++; // increase size of linked list
+        return true;
+    }
+
+    /**
+     * * Remove last node from linked list
+     *
+     * @return tail node of the list
+     */
+
+    public Optional<T> removeLast() {
+        tail = tail.prev;
+        //not last node of the list
+        if (tail != null) {
+            Node<T> temp = tail.next;
+            temp.prev = null;
+            tail.next = null;
+        }
+        size--;
+        return Optional.ofNullable(tail).isPresent() ? Optional.ofNullable(tail.getData()) : Optional.empty();
+    }
+
+    /**
+     * Add Node node at the start of the linked list.
+     *
+     * <p>Use {@link #addStart(Object node)} to add at the last linked list.
+     *
+     * @param data of Node add to start of the list
+     * @return true if added successfully
+     */
+    public boolean addStart(T data) {
+        Node<T> node = new Node(data);
+        if(isEmpty()) {
+            head = node;
+        } else {
+            // head points to later node, add node before the node that head pointer points to
+            node.next = head;
+            node.prev = null;
+            head.prev = node;
+            head = node; // update head pointer
+        }
+        size++;
+        return true;
+    }
+
+    /**
+     * Remove node from start of the linked list
+     *
+     * @return head of the list
+     */
+    public Optional<T> removeStart() {
+        head = head.next;
+        if(head != null) {
+            Node<T> temp = head.prev;
+            temp.next = null;
+            head.prev = null;
+        }
+        size--;
+        return Optional.ofNullable(head).isPresent() ? Optional.ofNullable(head.getData()) : Optional.empty();
+    }
+
+    /**
+     * Add Node node at the index (i th) of linked list.
+     *
+     * @param data to add
+     * @param index to insert in list
+     */
+    public void add(T data, int index) {
+        int length = getSize();
+        Node<T> node = new Node(data);
+
+        if (index < 0) {
+            throw new IllegalArgumentException("Invalid Index");
+        }
+        // give index more than length
+        if (index > length) {
+            throw new IndexOutOfBoundsException("Index Exceed List Length");
+        }
+        if (isEmpty()) { // Empty list
+            head = node;
+            tail = node;
+        } else if (index == 0) { // add first
+            node.next = head;
+            head.prev = node;
+            head = node;
+        } else if (index == length) { // add last
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        } else { // general case
+            Node<T> current = head;
+            while (index > 0 && current.next != null) {
+                current = current.next;
+                index--;
+            }
+            node.prev = current.prev;
+            current.prev.next = node;
+            node.next = current;
+            current.prev = node;
+        }
+        size++;
+    }
+
+    /**
+     *
+     * @param index to remove
+     * @return the element previously at the specified position (deleted one)
+     */
+    public Optional<T> remove(int index) {
+        int length = getSize();
+        Node<T> deletedNode;
+        if (isEmpty()) {
+            throw new RuntimeException("List is empty");
+        }
+        if (index < 0 || index > length - 1) {
+            throw new IllegalArgumentException("Invalid Index");
+        }
+        if (index == 0) { // remove first
+            deletedNode = head;
+            removeStart();
+        } else if (index == length) { // remove last
+            deletedNode = tail;
+            removeLast();
+        } else { // general case
+            Node<T> deletePoint = head;
+            // find element at index
+            while (index > 0) {
+                deletePoint = deletePoint.next;
+                index--;
+            }
+            deletedNode = deletePoint;
+            Node<T> previous = deletePoint.prev;
+            // set up previous and next node
+            previous.next = deletePoint.next;
+            deletePoint.next.prev = previous;
+            // remove
+            deletePoint.prev = null;
+            deletePoint.next = null;
+            size--;
+        }
+        return Optional.ofNullable(deletedNode).isPresent() ? Optional.ofNullable(deletedNode.getData()) : Optional.empty();
+    }
+
+    /**
+     *
+     * Add Node node at the index (i th) of linked list.
+     *
+     * <p>Use {@link #add(Node node, int index)} to add at the last linked list.
+     *
+     * @param node to add
+     * @param index to insert
+     * @return true if add successfully.
+     */
+//    public void optimizedAdd(Node node, int index) {
+//        int length = getSize();
+//        System.out.println("index is: " +  index);
+//        System.out.println("length is: " +  length);
+//
+//        if(isEmpty()){
+//            firstNode(node);
+//            size++;
+//        } else if (index < 0) {
+//            System.out.println("Invalid index");
+//        } else {
+//            if (index == 0) {
+//                // add first
+//                addStart(node);
+//            } else if (index == length) {
+//                // add last
+//                addLast(node);
+//            } else if (index + 1 > length){
+//                // if index larger than the whole list
+//                System.out.println("Invalid index: exceed the length");
+//            } else {
+//                // if index between 0 and size, then find the position
+//                int middleIndex = length / 2;
+//                Boolean firstHalf = (index < middleIndex) ? true : false;
+//                Node insertPoint = (firstHalf) ? head : tail;
+//                int step = (firstHalf) ? index : length - index - 1;
+//                while(step > 0) {
+//                    insertPoint = (firstHalf) ? insertPoint.next : insertPoint.prev;
+//                    step--;
+//                }
+//                Node pointBefore = insertPoint.prev;
+//                //setup with previous node
+//                pointBefore.next = node;
+//                node.prev = pointBefore;
+//                // set up with the node after
+//                node.next = insertPoint;
+//                insertPoint.prev = node;
+//                size++;
+//            }
+//        }
+//    }
+
+}
+
+
---
+++ java-practice/src/test/java/com/basic/collection/TestLinkedList.java
@@ -0,0 +1,302 @@
+package com.basic.collection;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// removeLast necessary to be Optional<Node<T>> and here use String
+// if removeLast uses Optional<Node> or Optional<LinkedList.Node> is wrong
+// LinkedList.Node means LinkedList.Node
+/**
+ * Test class for testing linked list methods
+ * This class is not thread safe
+ *
+ * @author Yongdi Wang
+ *
+ */
+
+public class TestLinkedList {
+    LinkedList linkedList;
+
+    @Nested
+    @DisplayName("Linked list tests")
+    class TestLinkedListDetail {
+        @BeforeEach
+        @DisplayName("Set up linked list")
+        void init() {
+            linkedList = new LinkedList();
+            // initialize a node which is not in linkedlist
+            // LinkedList.Node node = linkedList.new Node("First Node");
+            assertTrue(linkedList.isEmpty());
+        }
+
+
+        @Test
+        @DisplayName("Add Node at the start of the linked list")
+        void shouldAddNodeAtStartOfLinkedList() {
+            linkedList.addStart("First Node");
+            linkedList.addStart("Second Node");
+            linkedList.addStart("Third Node");
+            assertEquals(3, linkedList.getSize());
+            Optional<String> rlt = linkedList.getNode(0);
+            assertEquals("Third Node", rlt.isPresent() ? rlt.get() : Optional.empty());
+            Iterator<String> iterator = linkedList.iterator();
+            if (iterator.hasNext()) {
+                Optional<String> rlt2 = linkedList.getNode(0);
+                assertEquals("Second Node", rlt2.isPresent() ?  iterator.next() : Optional.empty());
+                assertEquals("First Node", rlt2.isPresent() ?  iterator.next() : Optional.empty());
+            }
+        }
+
+        @Test
+        @DisplayName("Add Node at the last of the linked list")
+        void shouldAddNodeAtLastOfLinkedList() {
+            linkedList.addLast("First Node");
+            linkedList.addLast("Second Node");
+            linkedList.addLast("Third Node");
+            assertEquals(3, linkedList.getSize());
+            Optional<String> rlt = linkedList.getNode(0);
+            assertEquals("First Node", rlt.isPresent() ? rlt.get() : Optional.empty());
+            Iterator<String> iterator = linkedList.iterator();
+            if (iterator.hasNext()) {
+                Optional<String> rlt2 = linkedList.getNode(0);
+                assertEquals("Second Node", rlt2.isPresent() ?  iterator.next() : Optional.empty());
+                assertEquals("Third Node", rlt2.isPresent() ?  iterator.next() : Optional.empty());
+            }
+        }
+
+        @Test
+        @DisplayName("Add Node at invalid index of the linked list")
+        void shouldThrowExceptionWhenAddNodeAtInvalidIndex() {
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                linkedList.add("First Node", -1);
+            });
+            assertEquals("Invalid Index", exception.getMessage());
+        }
+
+        @Test
+        @DisplayName("Add Node at index larger than length of the linked list")
+        void shouldThrowExceptionWhenAddNodeAtOutOfRangeIndex() {
+            Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
+                linkedList.add("First Node", 10);
+            });
+            assertEquals("Index Exceed List Length", exception.getMessage());
+        }
+
+        @Test
+        @DisplayName("Add Node at valid index in order -- add last")
+        void shouldAddNodeAtValidIndexACS() {
+            linkedList.add("First Node", 0);
+            linkedList.add("Second Node", 1);
+            linkedList.add("Third Node", 2);
+            assertEquals(3, linkedList.getSize());
+            Optional<String> rlt = linkedList.getNode(0);
+            assertEquals("First Node", rlt.isPresent() ? rlt.get() : Optional.empty());
+            Iterator<String> iterator = linkedList.iterator();
+            if (iterator.hasNext()) {
+                Optional<String> rlt2 = linkedList.getNode(0);
+                assertEquals("Second Node", rlt2.isPresent() ?  iterator.next() : Optional.empty());
+                assertEquals("Third Node", rlt2.isPresent() ?  iterator.next() : Optional.empty());
+            }
+        }
+
+        @Test
+        @DisplayName("Add Node at valid index in random order -- add at first")
+        void shouldAddNodeAtValidIndexInOrderDESC() {
+            linkedList.add("First Node", 0);
+            linkedList.add("Second Node", 0);
+            linkedList.add("Third Node", 0);
+            assertEquals(3, linkedList.getSize());
+            Optional<String> rlt = linkedList.getNode(0);
+            assertEquals("Third Node", rlt.isPresent() ? rlt.get() : Optional.empty());
+            Iterator<String> iterator = linkedList.iterator();
+            if (iterator.hasNext()) {
+                Optional<String> rlt2 = linkedList.getNode(0);
+                assertEquals("Second Node", rlt2.isPresent() ?  iterator.next() : Optional.empty());
+                assertEquals("First Node", rlt2.isPresent() ?  iterator.next() : Optional.empty());
+            }
+        }
+
+        @Test
+        @DisplayName("Add Node at valid index in random order -- add in between")
+        void shouldAddNodeAtValidIndexInRandomOrder() {
+            linkedList.add("First Node", 0);
+            linkedList.add("Second Node", 1);
+            linkedList.add("Third Node", 2);
+            linkedList.add("Forth Node", 1);
+            assertEquals(4, linkedList.getSize());
+            Optional<String> rlt = linkedList.getNode(0);
+            assertEquals("First Node", rlt.isPresent() ? rlt.get() : Optional.empty());
+            Iterator<String> iterator = linkedList.iterator();
+            if (iterator.hasNext()) {
+                Optional<String> rlt2 = linkedList.getNode(0);
+                assertEquals("Forth Node", rlt2.isPresent() ?  iterator.next() : Optional.empty());
+                assertEquals("Second Node", rlt2.isPresent() ?  iterator.next() : Optional.empty());
+                assertEquals("Third Node", rlt2.isPresent() ?  iterator.next() : Optional.empty());
+            }
+        }
+
+        @Test
+        @DisplayName("Get node at invalid index - exception")
+        void shouldThrowExceptionWhenIndexInvalid() {
+            linkedList.add("First Node", 0);
+            assertEquals(1, linkedList.getSize());
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                linkedList.getNode(-1);
+            });
+            assertEquals("Invalid Index", exception.getMessage());
+        }
+
+        @Test
+        @DisplayName("Get node at valid index - return data")
+        void shouldGetNodeDataAtIndex() {
+            linkedList.add("First Node", 0);
+            assertEquals(1, linkedList.getSize());
+            Optional<String> rlt = linkedList.getNode(0);
+            assertEquals("First Node", rlt.isPresent() ? rlt.get() : Optional.empty());
+        }
+
+        @Test
+        @DisplayName("Get node at index exceeded list length - null")
+        void shouldReturnNullWhenIndexExceedLength() {
+            linkedList.add("First Node", 0);
+            assertEquals(1, linkedList.getSize());
+            Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
+                linkedList.getNode(3);
+            });
+            assertEquals("Index Exceed List Length", exception.getMessage());
+        }
+
+        @Test
+        @DisplayName("Get node at last of list - empty list")
+        void shouldAddNodeAtLastToEmptyLinkedList() {
+            linkedList.addLast("First Node");
+            assertEquals(1, linkedList.getSize());
+            Optional<String> rlt = linkedList.getNode(0);
+            assertEquals("First Node", rlt.isPresent() ? rlt.get() : Optional.empty());
+        }
+
+        @Test
+        @DisplayName("Get node at last of list - non empty list")
+        void shouldAddNodeAtLastToNonEmptyLinkedList() {
+            linkedList.add("First Node",0);
+            linkedList.addLast("Second Node");
+            assertEquals(2, linkedList.getSize());
+            Optional<String> rlt = linkedList.getNode(0);
+            assertEquals("First Node", rlt.isPresent() ? rlt.get() : Optional.empty());
+            // needs to specify type like String??
+            Iterator<String> iterator = linkedList.iterator();
+            if (iterator.hasNext()) {
+                assertEquals("Second Node", rlt.isPresent() ?  iterator.next() : Optional.empty());
+            }
+            Exception exception = assertThrows(NoSuchElementException.class, () -> {
+                iterator.next();
+            });
+            assertEquals("No Such Element", exception.getMessage());
+        }
+
+        @Test
+        @DisplayName("Get middle node of the list - odd")
+        void shouldGetMiddleNodeWhenListHasOddNodes() {
+            linkedList.addLast("First Node");
+            linkedList.addLast("Second Node");
+            linkedList.addLast("Third Node");
+            assertEquals(3, linkedList.getSize());
+            Optional<String> rlt = linkedList.getMiddle();
+            assertEquals("Second Node", rlt.isPresent() ? rlt.get() : Optional.empty());
+        }
+
+        @Test
+        @DisplayName("Get middle node of the list - even")
+        void shouldGetMiddleNodeWhenListHasEvenNodes() {
+            linkedList.addLast("First Node");
+            linkedList.addLast("Second Node");
+            linkedList.addLast("Third Node");
+            linkedList.addLast("Forth Node");
+            assertEquals(4, linkedList.getSize());
+            Optional<String> rlt3 = linkedList.getMiddle();
+            assertEquals("Third Node", rlt3.isPresent() ? rlt3.get() : Optional.empty());
+        }
+
+        @Test
+        @DisplayName("Empty list from last")
+        void shouldEmptyListByRemoveLastNode() {
+            linkedList.addLast("First Node");
+            assertEquals(1, linkedList.getSize());
+            Optional<String> rlt = linkedList.removeLast();
+            assertEquals(Optional.empty(), rlt.isPresent() ? rlt.get() : Optional.empty());
+            assertEquals(0, linkedList.getSize());
+        }
+
+        @Test
+        @DisplayName("Empty list from last")
+        void shouldRemoveNodeFromLastofList() {
+            linkedList.addLast("First Node");
+            linkedList.addLast("Second Node");
+            assertEquals(2, linkedList.getSize());
+
+            Optional<String> rlt = linkedList.removeLast();
+            assertEquals("First Node", rlt.isPresent() ? rlt.get() : Optional.empty());
+            assertEquals(1, linkedList.getSize());
+        }
+
+        @Test
+        @DisplayName("Empty list from start")
+        void shouldEmptyListByRemoveFirstNode() {
+            linkedList.addStart("First Node");
+            assertEquals(1, linkedList.getSize());
+            Optional<String> rlt = linkedList.removeStart();
+            assertEquals(Optional.empty(), rlt.isPresent() ? rlt.get() : Optional.empty());
+            assertEquals(0, linkedList.getSize());
+        }
+
+        @Test
+        @DisplayName("Empty list from last")
+        void shouldRemoveNodeFromStartofList() {
+            linkedList.addLast("First Node");
+            linkedList.addLast("Second Node");
+            assertEquals(2, linkedList.getSize());
+
+            Optional<String> rlt = linkedList.removeStart();
+            assertEquals("Second Node", rlt.isPresent() ? rlt.get() : Optional.empty());
+            assertEquals(1, linkedList.getSize());
+        }
+
+        @Test
+        @DisplayName("Remove only node at index")
+        void shouldRemoveOnlyNodeAtIndexofList() {
+            linkedList.add("First Node", 0);
+            assertEquals(1, linkedList.getSize());
+            Optional<String> removedNode = linkedList.remove(0);
+            assertEquals("First Node", removedNode.get());
+            assertEquals(0, linkedList.getSize());
+        }
+
+        @Test
+        @DisplayName("Remove node at index")
+        void shouldRemoveNodeAtIndexofList() {
+            linkedList.add("First Node", 0);
+            linkedList.add("Second Node", 1);
+            linkedList.add("Third Node", 2);
+            assertEquals(3, linkedList.getSize());
+            Optional<String> removedNode = linkedList.remove(1);
+            assertEquals("Second Node", removedNode.get());
+            assertEquals(2, linkedList.getSize());
+            Optional<String> rlt = linkedList.getNode(0);
+            assertEquals("First Node", rlt.isPresent() ? rlt.get() : Optional.empty());
+            Iterator<String> iterator = linkedList.iterator();
+            if (iterator.hasNext()) {
+                Optional<String> rlt2 = linkedList.getNode(0);
+                assertEquals("Third Node", rlt2.isPresent() ?  iterator.next() : Optional.empty());
+            }
+        }
+
+    }
+}
